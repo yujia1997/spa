cd3b7e351049f6e8922fb878fcd6403cb9246f33
= <=>
2019-04-12 09:18:07 +0800

    密码可见

---
+++ 11-password-visual/password.js
@@ -0,0 +1,33 @@
+
+$(function(){
+    var $pwd     = $('#pwd-normal'),
+        $pwdText = $('#pwd-display'),
+        $btnTest = $('#test-button');
+
+    $pwd.keydown(function() {
+          $pwdText.val($pwd.val());
+            
+    });
+
+    $btnTest.click(function(){
+          var msg, pwd = $pwd.val();
+              
+          if(pwd.length < 6) {
+                  msg = '你的密码长度少于六位，强度较弱！';
+                      
+          } else if(/[a-zA-Z]/.test(pwd) == false) {
+                  msg = '你的密码只包含数字，强度较弱!';
+                      
+          } else if(/\d/.test(pwd) == false){
+                  msg = '你的密码只包含字母，强度较弱！';
+                      
+          } else {
+                  msg = '你的密码很强壮！';
+                      
+          }
+
+              alert(msg);
+                
+    });
+
+});
